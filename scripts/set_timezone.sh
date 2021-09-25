6b30967375f61bd115c6fa3b0a57038429348b2e
John Ajera <37360952+jajera@users.noreply.github.com>
2021-09-26 02:46:16 +0800

    Update set_timezone.sh

--- scripts/set_timezone.sh
+++ scripts/set_timezone.sh
@@ -3,3 +3,4 @@ uci set system.@system[0].zonename=$ZONENAME;
 uci commit system;
 /etc/init.d/system restart;
 echo $(date +%Z);
+echo $(cat /etc/config/system | grep "option zonename" | cut -d "'" -f 2);
