163ab67fa11e566215ead386575b229eb44fb4a9
John Ajera <37360952+jajera@users.noreply.github.com>
2021-09-26 23:59:14 +0800

    Update new_fw_rule.sh

---
+++ scripts/new_fw_rule.sh
@@ -0,0 +1,9 @@
+uci set firewall."$FW_RULE_ID"='rule';
+uci set firewall."$FW_RULE_ID".name=$FW_RULE_NAME;
+uci set firewall."$FW_RULE_ID".src=$FW_RULE_SRC;
+uci set firewall."$FW_RULE_ID".dest_port=$FW_RULE_DEST_PORT;
+uci set firewall."$FW_RULE_ID".proto=$FW_RULE_DEST_PROTO;
+uci set firewall."$FW_RULE_ID".target=$FW_RULE_DEST_TARGET;
+uci commit firewall;
+/etc/init.d/firewall restart;
+uci -s show firewall."$FW_FORWARDING_NAME";
