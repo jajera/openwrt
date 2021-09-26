13c55edb39bc8039e9077493cb09d431fa5c42b6
John Ajera <37360952+jajera@users.noreply.github.com>
2021-09-26 23:00:50 +0800

    Update new_fw_zone.sh

--- scripts/new_fw_zone.sh
+++ scripts/new_fw_zone.sh
@@ -1,6 +1,9 @@
-uci set firewall."$FW_ZONE_NAME"='zone'
-uci set firewall."$FW_ZONE_NAME".name=$FW_ZONE_NAME
-uci set firewall."$FW_ZONE_NAME".network=$FW_ZONE_NETWORK
-uci set firewall."$FW_ZONE_NAME".input=$FW_ZONE_INPUT
-uci set firewall."$FW_ZONE_NAME".output=$FW_ZONE_OUTPUT
-uci set firewall."$FW_ZONE_NAME".forward=$FW_ZONE_FORWARD
+uci set firewall."$FW_ZONE_NAME"='zone';
+uci set firewall."$FW_ZONE_NAME".name=$FW_ZONE_NAME;
+uci set firewall."$FW_ZONE_NAME".network=$FW_ZONE_NETWORK;
+uci set firewall."$FW_ZONE_NAME".input=$FW_ZONE_INPUT;
+uci set firewall."$FW_ZONE_NAME".output=$FW_ZONE_OUTPUT;
+uci set firewall."$FW_ZONE_NAME".forward=$FW_ZONE_FORWARD;
+uci commit firewall
+/etc/init.d/firewall restart
+uci -s show firewall."$FW_ZONE_NAME"
