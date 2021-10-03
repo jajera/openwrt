d5ded53043da1b0a0f2b7bc9e81e7e078ed5c185
John Ajera <37360952+jajera@users.noreply.github.com>
2021-10-03 21:37:59 +0800

    updated

--- scripts/new_device.sh
+++ scripts/new_device.sh
@@ -1,6 +1,7 @@
-uci set network."$DEV_LABEL"_int=$DEV_CLASS;
-uci set network."$DEV_LABEL"_int.type=$DEV_TYPE;
-uci set network."$DEV_LABEL"_int.name=$DEV_NAME;
+uci set network."$DEV_LABEL".proto=$DEV_PROTO;
+uci set network."$DEV_LABEL".device=$DEV_NAME;
+uci set network."$DEV_LABEL".ipaddr=$DEV_IPADDR;
+uci set network."$DEV_LABEL".netmask=$DEV_NETMASK;
 uci commit network;
 /etc/init.d/network restart;
 uci -q show network.$DEV_LABEL;
