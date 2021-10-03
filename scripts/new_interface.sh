b628aeef99247c00c3055e2cc78138374779a6a8
John Ajera <37360952+jajera@users.noreply.github.com>
2021-10-03 22:18:16 +0800

    Update new_interface.sh

--- scripts/new_interface.sh
+++ scripts/new_interface.sh
@@ -1,8 +1,8 @@
-uci set network."$INT_LABEL"='interface';
-uci set network."$INT_LABEL".proto=$INT_PROTO;
-uci set network."$INT_LABEL".device=$DEV_NAME;
-uci set network."$DEV_LABEL".ipaddr=$INT_IPADDR;
-uci set network."$DEV_LABEL".netmask=$INT_NETMASK;
+uci set network."$INT_NAME"='interface';
+uci set network."$INT_NAME".proto=$INT_PROTO;
+uci set network."$INT_NAME".device=$DEV_NAME;
+uci set network."$INT_NAME".ipaddr=$INT_IPADDR;
+uci set network."$INT_NAME".netmask=$INT_NETMASK;
 uci commit network;
 /etc/init.d/network restart;
-uci -q show network.$INT_LABEL;
+uci -q show network.$INT_NAME;
