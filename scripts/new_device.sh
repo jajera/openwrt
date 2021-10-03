8bf7a49d38e5dc45d4997eac13c9295d00252120
John Ajera <37360952+jajera@users.noreply.github.com>
2021-10-03 22:07:32 +0800

    Update new_device.sh

--- scripts/new_device.sh
+++ scripts/new_device.sh
@@ -1,6 +1,6 @@
 uci set network."$DEV_LABEL"='device';
 uci set network."$DEV_LABEL".type=$DEV_TYPE;
-uci set network."$INT_LABEL".name=$DEV_NAME;
+uci set network."$DEV_LABEL".name=$DEV_NAME;
 uci commit network;
 /etc/init.d/network restart;
 uci -q show network.$DEV_LABEL;
